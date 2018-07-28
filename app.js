ab19070617866c066187732e496e9b624083974f
Nishant <nishantsubedi1996@gmail.com>
2018-07-28 20:19:20 +0545

    initial setup

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const ejs = require('ejs');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/university', function(err){
+    if (err) throw err
+  else console.log("connected to mongodb"); 
+});
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(bodyParser()); // pull information from html in POST
+app.use('/public',express.static(path.join(__dirname, 'public')));
+
+
+app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'));
